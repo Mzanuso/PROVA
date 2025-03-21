714d3ca2dbbfed0e0b3b19df2b655f4188539ab0
Mzanuso <mzanuso@gmail.com>
2025-03-20 20:44:23 -0400

    Add files via upload

---
+++ js/webpack-78956a3d7ad726d1.js
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="robots" content="noindex, nofollow" />
+    <link rel="canonical" href="https://app.ltx.studio" />
+    <link rel="icon" type="image/png" href="/favicon.png" />
+    <title>Storytelling Transformed | LTX Studio</title>
+    <meta
+      name="description"
+      content="LTX Studio is a holistic, AI-driven filmmaking platform for creators, marketers, filmmakers and studios. From ideation to production, LTX Studio enables you to control every aspect of your story, allowing elevated outcomes that actualize your vision."
+    />
+    <script
+      src="https://cdn.optimizely.com/js/27444860214.js"
+      async="true"
+      data-nscript="afterInteractive"
+    ></script>
+    <script>
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://gtm.ltx.studio/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-TZ4LHS2");
+    </script>
+    <!-- Accessibility start for ltx.studio -->
+    <script>
+      window.addEventListener("DOMContentLoaded", () => {
+        window.interdeal = {
+          sitekey: "9fb4a8f9ffee479f4b22e532651d9ab0",
+          Position: "left",
+          Menulang: "EN",
+          domains: {
+            js: "https://cdn.equalweb.com/",
+            acc: "https://access.equalweb.com/",
+          },
+        };
+        (function (doc, head, body) {
+          var coreCall = doc.createElement("script");
+          coreCall.src = interdeal.domains.js + "core/4.6.3/accessibility.js";
+          coreCall.defer = true;
+          coreCall.integrity =
+            "sha512-+5lbZsIsOqyfEswqMIHyOrR4jrrBUQ0aVv0KYYoZ6/jTkKsTIqAHwkHgFEoRDM3NbjZ0lOxR0qSslbA3NIXrfw==";
+          coreCall.crossOrigin = "anonymous";
+          coreCall.setAttribute("data-cfasync", true);
+          body ? body.appendChild(coreCall) : head.appendChild(coreCall);
+        })(document, document.head, document.body);
+      });
+    </script>
+    <style>
+      #INDmenu-btn {
+        display: none !important;
+      }
+    </style>
+    <!-- Accessibility end for ltx.studio -->
+    <script type="module" crossorigin src="/assets/index-Bc-Ry2Sr.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-9utwllq5.css">
+  </head>
+  <body>
+    <div id="root"></div>
+    <div id="topmost-modal-root"></div>
+  </body>
+</html>
